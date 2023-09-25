dd4b919fe122596dcd6ce9ed6b78743c82bb971e
«Eduard» <eduardo-capoeirista@yandex.ru>
2023-09-25 15:51:34 +0300

    task 18 added

---
+++ task18/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 18: Напишите программу,которая по заданному номеру четверти показывает диапозон возможных координат
+точек в этой четверти (x и y)
+*/
+
+void PrintRange(int part)
+{
+    if (part == 1)
+{
+    System.Console.WriteLine("x>0, y>0");
+}
+else if (part == 2)
+{
+    System.Console.WriteLine("x<0, y>0");
+}
+else if (part == 3)
+{
+    System.Console.WriteLine("x<0, y<0");
+}else if (part == 4)
+{
+    System.Console.WriteLine("x>0, y>0");
+}
+else
+{
+    System.Console.WriteLine("error");
+}
+}
+int part = Convert.ToInt32(Console.ReadLine());
+PrintRange(part);
