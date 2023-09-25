25c2df903c34b689b3d60dedfd22502eb3115499
«Eduard» <eduardo-capoeirista@yandex.ru>
2023-09-25 09:43:19 +0300

    task00 added

---
+++ task00/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+Напишите программу, которая на вход принимает число и выдаёт его квадрат
+*/
+
+Console.WriteLine("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine(result);
