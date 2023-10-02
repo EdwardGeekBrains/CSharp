e3b1dec53f1078ba80a17ccabd59c520dff0b046
«Eduard» <eduardo-capoeirista@yandex.ru>
2023-10-02 14:15:37 +0300

    task24 added

---
+++ task24/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+int SumFrom1ToN (int n)
+{
+    int sum = 0;
+    for (int i = 1; i<= n; i++)
+    {
+        sum += i;
+    }
+  return sum;
+}
+
+int SumFrom1ToNFast(int n)
+{
+    int sum = (1 + n) * n /2;
+    return sum;
+}
+
+int n = 8;
+Console.WriteLine(SumFrom1ToN(n));
+Console.WriteLine(SumFrom1ToNFast(n));
